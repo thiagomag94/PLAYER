eb01f585b0374fa6dc1a6861f073b3050c4caa09
thiagomag94 <thiagomag94@gmail.com>
2022-10-24 09:13:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var buttonPlayAll = document.getElementById('playAll');
+buttonPlayAll.addEventListener("click", () =>{
+    var audio = document.getElementById('1');
+    audio.play();
+    return console.log('lido')
+})
