847ca22acfb24ef0b38dc29afda3265deedbb81f
thiagomag94 <thiagomag94@gmail.com>
2022-10-25 13:45:13 -0300

    barra de progresso funcionando

--- script.js
+++ script.js
@@ -4,15 +4,18 @@
 var audio = document.getElementById('1');
 var audio2 = document.getElementById('2');
 var audio3 = document.getElementById('3');
+var musicName = document.getElementById('music-name');
 var currentTimeSpan = document.getElementById('current');
 var durationTimeSpan = document.getElementById('duration')
+var label01 = document.getElementById('music01')
+var label02 = document.getElementById('music02')
 
 
-//var album ={
-  //  'musica01':{'audio': audio, 'name':'Interrupted Light', 'currentTime': audio.currentTime, 'duration':audio.duration},
-  //  'musica02':{'audio': audio2, 'name':'Dark Memories', 'currentTime': audio2.currentTime, 'duration':audio2.duration},
+var album ={
+    'musica01':{'audio': audio, 'name':'Interrupted Light', 'currentTime': audio.currentTime, 'duration':audio.duration},
+    'musica02':{'audio': audio2, 'name':'Dark Memories', 'currentTime': audio.currentTime, 'duration':audio2.duration},
 
-//}
+}
 
 
 // -----------------------------Seta os estados como iniciais---
@@ -21,6 +24,10 @@ var durationTimeSpan = document.getElementById('duration')
 var stateButton = 0
 //guarda o estado do footer (0 - não aparece, 1 - aparece)
 var stateFooter = 0
+
+var stateLista = 0
+
+
 
 
 
@@ -48,35 +55,46 @@ buttonPlayAll.addEventListener("click", () =>{
     }
     else {
         stateButton = 1
-        audio.play();
-        showFooter()
+        showFooter('musica01', album.musica01.name);
+        album.musica01.audio.play();
+        
         console.log('showing...')
     }
+
+    progressBar('musica01')
 })
 
 //------------------------------------Aparecer Footer-----------------------
 
-const showFooter = () => {
-    displayFooter = document.getElementById('audio-player-container')
+const showFooter = (music, music_name) => {
+    var displayFooter = document.getElementById('audio-player-container')
+    musicName.innerText = music_name
+    playPause(music)
     displayFooter.classList.add('transform', '-translate-y-0')
     displayFooter.style.transition = 'all 1.618s ease'
+    stateFooter = 1
+    
 }
 
-const hideFooter = () => {
+const hideFooter = (music) => {
     displayFooter = document.getElementById('audio-player-container')
+    playPause(music)
     displayFooter.classList.remove('transform', '-translate-y-0')
     displayFooter.style.transition = 'all 1.618s ease'
+    stateFooter = 0
 }
 
 //------------------------Atualiza Progress Bar ----------------------------
 
-var progressBar = document.getElementById('seek-slider');
-audio.addEventListener('timeupdate', function(){
-    progressBar.setAttribute("value", this.currentTime / this.duration);
-    currentTimeSpan.textContent = `${calculateTime(this.currentTime)} `
-    durationTimeSpan.textContent = `${calculateTime(this.duration)}`
+function progressBar (music) {
+    var progressBar = document.getElementById('seek-slider');
+    album[music].audio.addEventListener('timeupdate', function(){
+        progressBar.setAttribute("value", this.currentTime / this.duration);
+        currentTimeSpan.textContent = `${calculateTime(this.currentTime)} `
+        durationTimeSpan.textContent = `${calculateTime(this.duration)}`
 
 })
+}
 
 //-------------------Calcular segundos e minutos-----------------------
 
@@ -89,15 +107,56 @@ const calculateTime = (secs) => {
 
 //--------------------muda botão play/pause---------------------------
 
-var playIcon = document.getElementById('play-icon');
-playIcon.addEventListener('click', () =>{
-    if (playIcon.textContent == 'play_arrow'){
-        playIcon.textContent = 'pause';
-        audio.pause()
+function playPause(music_name){
+    var playIcon = document.getElementById('play-icon');
+    playIcon.addEventListener('click', () =>{
+    if (playIcon.textContent == 'pause'){
+        playIcon.textContent = 'play_arrow';
+        album[music_name].audio.pause()
     }
     else {
-        playIcon.textContent = 'play_arrow';
-        audio.play()
+        playIcon.textContent = 'pause';
+        album[music_name].audio.play()
+    }
+    
+})
+}
+
+// ----------------------------toca musica 01 -------------------------
+
+label01.addEventListener('click', () =>{
+    hideFooter('musica02');
+    if (stateLista==0){
+        showFooter('musica01',album.musica01.name);
+        progressBar('musica01');
+        album.musica01.audio.play();
+        label01.classList.add('bg-slate-800');
+        stateLista = 1;
+    }
+    else {
+        album.musica01.audio.pause();
+        
+        stateLista = 0;
+    }
+
+    console.log('showing...')
+})
+
+//------------------------------toca musica 02 ---------------------
+
+label02.addEventListener('click', () =>{
+    hideFooter('musica01');
+    console.log(stateLista);
+    if (stateLista==0){
+        showFooter('music02', album.musica02.name);
+        progressBar('musica02');
+        album.musica02.audio.play();
+        label02.classList.add('bg-slate-800');
+        stateLista = 1;
+    }
+    else {
+        label02.classList.remove('bg-slate-800');
+        stateLista = 0;
     }
     
 })
