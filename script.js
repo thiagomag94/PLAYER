9bd052f65a115423359101ec0f7e9f819602508e
thiagomag94 <thiagomag94@gmail.com>
2022-10-24 23:12:01 -0300

    barra de progresso funcionando

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 
+//----------------------Define os audios e suas propriedades-------------------------
+
+var audio = document.getElementById('1');
+var audio2 = document.getElementById('2');
+var audio3 = document.getElementById('3');
+var currentTimeSpan = document.getElementById('current');
+var durationTimeSpan = document.getElementById('duration')
+
+
+//var album ={
+  //  'musica01':{'audio': audio, 'name':'Interrupted Light', 'currentTime': audio.currentTime, 'duration':audio.duration},
+  //  'musica02':{'audio': audio2, 'name':'Dark Memories', 'currentTime': audio2.currentTime, 'duration':audio2.duration},
+
+//}
+
 
 // -----------------------------Seta os estados como iniciais---
 
@@ -21,14 +36,14 @@ var buttonPlayAll = document.getElementById('playAll');
 
 
 buttonPlayAll.addEventListener("click", () =>{ 
-    var audio = document.getElementById('1');
+    
     
     
     if (stateButton === 1){
         stateButton = 0
-        audio.pause()
+        //audio.pause()
         console.log('hiding..')
-        hideFooter();
+        
 
     }
     else {
@@ -53,4 +68,36 @@ const hideFooter = () => {
     displayFooter.style.transition = 'all 1.618s ease'
 }
 
+//------------------------Atualiza Progress Bar ----------------------------
+
+var progressBar = document.getElementById('seek-slider');
+audio.addEventListener('timeupdate', function(){
+    progressBar.setAttribute("value", this.currentTime / this.duration);
+    currentTimeSpan.textContent = `${calculateTime(this.currentTime)} `
+    durationTimeSpan.textContent = `${calculateTime(this.duration)}`
+
+})
+
+//-------------------Calcular segundos e minutos-----------------------
+
+const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
+}
+
+//--------------------muda botão play/pause---------------------------
+
+var playIcon = document.getElementById('play-icon');
+playIcon.addEventListener('click', () =>{
+    if (playIcon.textContent == 'play_arrow'){
+        playIcon.textContent = 'pause';
+        audio.pause()
+    }
+    else {
+        playIcon.textContent = 'play_arrow';
+        audio.play()
+    }
     
+})
